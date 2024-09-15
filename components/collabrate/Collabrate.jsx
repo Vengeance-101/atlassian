47b4cad25d15b8d02f3773e15940510eb9066707
Vishal <157721983+Vengeance-101@users.noreply.github.com.>
2024-09-15 22:00:51 +0530

    updated

--- components/collabrate/Collabrate.jsx
+++ components/collabrate/Collabrate.jsx
@@ -3,11 +3,11 @@ import Button from "@/lib/button/Button";
 import { colabrate } from "@/data/collaborate/CollabrateData";
 const Collabrate = () => {
   return (
-    <div className="px-[11rem]  h-auto ">
+    <div className=" max-md:px-12 px-[11rem] w-full max-sm:px-6  h-auto ">
       <p className="text-left w-full text-2xl py-5 font-extrabold text-black">
         Collaborate
       </p>
-      <div className="grid grid-cols-2 gap-12 w-full h-auto py-10">
+      <div className="grid grid-cols-2 max-lg:gap-5 max-lg:grid-cols-1 gap-12 w-full h-auto py-10">
         {colabrate.map((item) => {
           return (
             <div key={item.title}>
--- components/powerful/PowerFull.jsx
+++ components/powerful/PowerFull.jsx
@@ -3,14 +3,14 @@ import Button from "@/lib/button/Button";
 import React from "react";
 const PowerFull = () => {
   return (
-    <div className="px-[11rem]  h-auto ">
+    <div className="px-[11rem] max-md:px-12 max-sm:px-6 w-full h-auto ">
       <h1 className="text-center text-[2.5rem] py-5 font-extrabold text-black">
         Powerful products for every team
       </h1>
       <p className="text-left w-full text-2xl py-5 font-extrabold text-black">
         Powerful products for every team
       </p>
-      <div className="grid grid-cols-2 gap-12 w-full h-auto py-10">
+      <div className="grid grid-cols-2 gap-12 max-lg:gap-5 max-lg:grid-cols-1 w-full h-auto py-10">
         {PowerData.map((item) => {
           return (
             <div key={item.title}>
