11363203a8aeb0ed2a1a4f8d13d87dc3c7903f3b
Vishal <157721983+Vengeance-101@users.noreply.github.com.>
2024-09-22 00:26:50 +0530

    updated

--- components/revealcreen/LoadingScreen.jsx
+++ components/revealcreen/LoadingScreen.jsx
@@ -15,17 +15,13 @@ export default function LoadingScreen({ setContentLoad }) {
         if (oldProgress >= 100) {
           clearInterval(interval);
           const tl = gsap.timeline();
-          tl.to(progressRef.current, {
+          tl.to(".loading-counter", {
             opacity: 0,
-            duration: 0.5,
           })
-            .to(".loading-counter", {
-              opacity: 0,
-            })
             .to(loadingScreen.current, {
-              width: 0,
               delay: 0.2,
-              duration: 1,
+              duration: 0.2,
+              opacity: 0,
               ease: "power2.out",
             })
             .to(loadingScreen.current, {
@@ -39,23 +35,23 @@ export default function LoadingScreen({ setContentLoad }) {
         }
         return oldProgress + 1;
       });
-    }, 25); // Adjust interval for loading speed
+    }, 15); // Adjust interval for loading speed
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div
       ref={loadingScreen}
-      className="fixed top-0 left-0 h-full w-full flex flex-col items-center justify-center bg-black text-white z-[999] transition-opacity duration-700"
+      className="fixed  h-full w-full flex flex-col items-center justify-center bg-black  z-[999] transition-opacity duration-700"
     >
       <div
         ref={progressRef}
-        className="h-[.3%]  bg-indigo-400 transition-all  duration-300 ease-out"
-        style={{ width: `${progress}%` }}
+        className=" bg-white transition-all w-full  duration-300 ease-out"
+        style={{ height: `${progress}%` }}
       ></div>
 
       {/* Progress Text */}
-      <div className="text-[6rem] font-bold loading-counter absolute top-[50%] left-[50%]  translate-x-[-50%]  translate-y-[-50%] ">
+      <div className="text-[8rem] font-bold loading-counter text-white mix-blend-difference absolute top-[50%] left-[50%]  translate-x-[-50%]  translate-y-[-50%] ">
         {progress}%
       </div>
     </div>
